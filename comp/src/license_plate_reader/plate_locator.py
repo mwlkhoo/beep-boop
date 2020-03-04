ad65a0dbf3c3b5f2a27ba35bb66392b24ec3c36f
fizzer <fizzer@example.com>
2020-03-03 21:42:46 -0800

    text detection

--- comp/src/license_plate_reader/plate_locator.py
+++ comp/src/license_plate_reader/plate_locator.py
@@ -12,37 +12,103 @@ from anki_vector.util import degrees, distance_mm, speed_mmps
 # Modify the SN to match your robot’s SN
 ANKI_SERIAL = '005040b7'
 ANKI_BEHAVIOR = av.connection.ControlPriorityLevel.OVERRIDE_BEHAVIORS_PRIORITY
+MIN_CONFIDENCE = 0.8
+OFFSET = 20
 
 class Plate_Locator(object):
     """docstring for ClassName"""
     def __init__(self):
-        frame_w = 320
-        frame_h = 320
-        self.dim = (frame_w, frame_h)
+        
+        # Desired shape
+        self.desired_w = 480
+        self.desired_h = 640
+        self.d_dim = (self.desired_w, self.desired_h)
 
+        self.mean = (123.68, 116.78, 103.94)
+        self.net = cv2.dnn.readNet("/home/fizzer/enph353_git/beep-boop/comp/src/license_plate_reader/frozen_east_text_detection.pb")
+        self.layerNames = ["feature_fusion/Conv_7/Sigmoid", "feature_fusion/concat_3"]
+       
     def main(self):
-        with av.Robot(serial=ANKI_SERIAL, behavior_control_level=ANKI_BEHAVIOR) as robot:
+        # with av.Robot(serial=ANKI_SERIAL, behavior_control_level=ANKI_BEHAVIOR) as robot:
 
-            print("running loop", flush=True)
+        #     print("running loop", flush=True)
 
-            robot.behavior.set_lift_height(1.0, 10.0, 10.0, 0.0, 3)
-            robot.behavior.set_head_angle(degrees(5.0))
+        #     robot.behavior.set_lift_height(1.0, 10.0, 10.0, 0.0, 3)
+        #     robot.behavior.set_head_angle(degrees(5.0))
 
-            robot.camera.init_camera_feed()
-            print("camera init success", flush=True)
+        #     robot.camera.init_camera_feed()
+        #     print("camera init success", flush=True)
 
-            while(True):
-                robot_cap = robot.camera.latest_image.raw_image
-                print("frame captured", flush=True)
+        while(True):
+            # robot_cap = robot.camera.latest_image.raw_image
+            # print("frame captured", flush=True)
 
-                frame = cv2.cvtColor(np.array(robot_cap), cv2.COLOR_BGR2GRAY)
-                # Make a copy of the frame
-                orig = frame.copy()
-                frame = cv2.resize(frame, self.dim, interpolation = cv2.INTER_AREA)
-                # frame_w = frame.shape[1]
-                # print("color to gray", flush=True)
-                # cv2.imshow("Raw img", frame)
-                k = cv2.waitKey(3)
+            frame = cv2.imread("/home/fizzer/Downloads/blue_plate.jpg")
+            frame_w = frame.shape[1]
+            frame_h = frame.shape[0]
+            dim = (frame_w, frame_h)
+            # cv2.imshow("frame", robot_cap)
+            # cv2.waitKey(5)
+            # frame = cv2.cvtColor(np.array(robot_cap), cv2.COLOR_BGR2RGB)
+            # # Make a copy of the frame
+            orig = frame.copy()
+            frame = cv2.resize(frame, self.d_dim, interpolation = cv2.INTER_AREA)
+            cv2.imshow("l", frame)
+            # frame_w = frame.shape[1]
+            # print("color to gray", flush=True)
+            # cv2.imshow("Raw img", frame)
+            # cv2.waitKey(5)
+            rW = float(frame_w) / float(self.desired_w)
+            rH = float(frame_h) / float(self.desired_h)
+
+            # construct a blob from the frame and then perform a forward pass
+            # of the model to obtain the two output layer sets
+            blob = cv2.dnn.blobFromImage(frame, 1.0, self.d_dim, self.mean, swapRB = True, crop = False)
+            self.net.setInput(blob)
+            (scores , geometry) = self.net.forward(self.layerNames)
+            # decode the predictions, then  apply non-maxima suppression to
+            # suppress weak, overlapping bounding boxes
+            (rects, confidences) = Plate_Locator.decode_predictions(scores, geometry)
+            boxes = non_max_suppression(np.array(rects), probs=confidences)
+            count_box = 0
+            # loop over the bounding boxes
+            for (startX, startY, endX, endY) in boxes:
+                # scale the bounding box coordinates based on the respective
+                # ratios
+                
+                startX = int(startX * rW)
+                startY = int(startY * rH)
+                endX = int(endX * rW)
+                endY = int(endY * rH)
+                # draw the bounding box on the frame
+                
+                if count_box == 0:
+                    plate_rhs = orig[startY:endY + OFFSET, startX:endX]  
+                    cv2.imwrite('plate_rhs.png', plate_rhs)
+                   
+                    
+
+                elif count_box == 1:
+                    plate_num = orig[startY:endY + OFFSET, startX:endX]
+                    cv2.imwrite('plate_num.png', plate_num)
+
+                else:
+                    plate_lhs = orig[startY:endY + OFFSET, startX:endX]
+                    cv2.imwrite('plate_lhs.png', plate_lhs)
+
+                cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+                count_box += 1
+
+
+
+            # show the output frame
+            cv2.imshow("Text Detection", orig)
+            key = cv2.waitKey(1) & 0xFF
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                break
+  			
+			
 
 
 
@@ -68,7 +134,7 @@ class Plate_Locator(object):
             for x in range(0, numCols):
                 # if our score does not have sufficient probability,
                 # ignore it
-                if scoresData[x] < args["min_confidence"]:
+                if scoresData[x] < MIN_CONFIDENCE:
                     continue
                 # compute the offset factor as our resulting feature
                 # maps will be 4x smaller than the input image
@@ -95,5 +161,5 @@ class Plate_Locator(object):
         # return a tuple of the bounding boxes and associated confidences
         return (rects, confidences)
 
-    if __name__ == "__main__":
-        Plate_Locator().main()
+if __name__ == "__main__":
+    Plate_Locator().main()
