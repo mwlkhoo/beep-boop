a60502a41d4ffa35fcc942c6e91ed1a2ed702e03
Michelle Khoo <michelle@khoolworks.com>
2020-03-12 18:48:08 -0700

    Added path detection and updated file structure

---
+++ comp/src/anki_control/control.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+import constants
+import detect
+
+def follow(state):
+	
--- comp/src/anki_control/main.py
+++ comp/src/anki_control/main.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
+import sys
 
 import anki_vector as av
 from anki_vector.util import degrees
 
 import constants
-import detection.crosswalk
+import detect
+import path_following
 
 # Modify the SN to match your robot’s SN
 ANKI_SERIAL = '005040b7'
@@ -28,26 +30,42 @@ class Anki(object):
             robot.behavior.set_head_angle(degrees(5.0))
             robot.camera.init_camera_feed()
 
-            # while(True):
-            #     raw_frame = robot.camera.latest_image.raw_image
-            #     raw_img = cv2.cvtColor(np.array(robot.camera.latest_image.raw_image), cv2.COLOR_BGR2RGB)
-            #     gr_img = cv2.cvtColor(np.array(raw_img), cv2.COLOR_BGR2GRAY)
-            #     cv2.imshow("Anki view", raw_img)
-            #     k = cv2.waitKey(1)
+            ########## PRINT AND GET IMAGES CONTINUOUSLY ############
+            while(True):
+                raw_frame = robot.camera.latest_image.raw_image
+                raw_img = cv2.cvtColor(np.array(robot.camera.latest_image.raw_image), cv2.COLOR_BGR2RGB)
+                gr_img = cv2.cvtColor(np.array(raw_img), cv2.COLOR_BGR2GRAY)
 
-            ############ TEST: READING FROM OTHER FILE ##############
-            # detection.crosswalk.eye_color(robot)
+                ################ PRINT IMG ARRAY ###################
+                # np.set_printoptions(threshold = sys.maxsize)
+                # print(gr_img)
 
-            ############ PRINT AND GET SINGLE IMAGE #################
+                print(detect.path(gr_img))
+
+                raw_img = cv2.rectangle(raw_img, (0, constants.path_init_H), (constants.W-1, constants.H-1), (255,0,0), 10)
+                cv2.imshow("Anki view", raw_img)
+                k = cv2.waitKey(-1)
+
+            # ########### PRINT AND GET SINGLE IMAGE #################
             raw_frame = robot.camera.latest_image.raw_image
             raw_img = cv2.cvtColor(np.array(raw_frame), cv2.COLOR_BGR2RGB)
+            gr_img = cv2.cvtColor(np.array(raw_img), cv2.COLOR_BGR2GRAY)
+            np.set_printoptions(threshold = sys.maxsize)
 
-            if(detection.crosswalk.detect(raw_img)):
-                robot.behavior.say_text("Crosswalk!")
+            # print(g)
+            # cv2.imshow("Anki view", raw_img)
+            # k = cv2.waitKey(-1)
 
-            while(True):
-                cv2.imshow("Anki view", raw_img)
-                k = cv2.waitKey(1)
+            ############ TEST: PATH FOLLOWING #######################
+
+
+            ############ TEST: CROSSWALK DETECTION ##################
+            # if(detect.crosswalk(raw_img)):
+            #     robot.behavior.say_text("Crosswalk!")
+
+            # while(True):
+            #     cv2.imshow("Anki view", raw_img)
+            #     k = cv2.waitKey(1)
            
 if __name__ == "__main__":
     Anki().main()
