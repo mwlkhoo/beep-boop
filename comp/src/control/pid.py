1087183f854a8cff5e7e02cdb44b931b0707ba24
Michelle Khoo <michelle@khoolworks.com>
2020-04-14 18:09:31 -0700

    Updated PID to be exonential

--- comp/src/control/pid.py
+++ comp/src/control/pid.py
@@ -4,20 +4,33 @@ import numpy as np
 
 # import constants
 
-P = 0.8
-P_ANG = 3.5
+# P = 0.8
+# P_ANG = 3.5
+
+# BASE_VEL = 0.1*P
+# SCALE_VEL = 0.04*P
+
+# SCALE_ANG = 0.05*P_ANG*P
+# CONST_ANG = 0.2*P_ANG*P
+
+P = 1.5
+P_ANG = 0.1
 
 BASE_VEL = 0.1*P
-SCALE_VEL = 0.04*P
+SCALE_VEL = 0.02*P
 
-SCALE_ANG = 0.05*P_ANG*P
-CONST_ANG = 0.2*P_ANG*P
+SCALE_ANG = 1.8
+CONST_ANG = 0.001
 
 def update(move, state):
     
     if(not sum(state) is -2):
-        move.linear.x = BASE_VEL - abs(state[1]-state[0])*SCALE_VEL
-        move.angular.z = (state[1]-state[0])*SCALE_ANG
+    	diff = state[1]-state[0]
+        move.linear.x = BASE_VEL - abs(diff)*SCALE_VEL
+        if diff is 0:
+        	move.angular.z = 0
+       	else:
+        	move.angular.z = (SCALE_ANG**abs(diff))*P_ANG*diff
     else:
         move.linear.x = 0
         move.angular.z = CONST_ANG
