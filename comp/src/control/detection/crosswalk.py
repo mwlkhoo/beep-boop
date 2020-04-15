01eadc213c5f30d2c6f1974c0fcd34f92660b6cc
Michelle Khoo <michelle@khoolworks.com>
2020-04-14 23:02:15 -0700

    Updated crosswalk detection (wider range) and added pid option

--- comp/src/control/detection/crosswalk.py
+++ comp/src/control/detection/crosswalk.py
@@ -4,18 +4,19 @@ import time
 
 import constants
 
-ROW_LIM_STOP = 120
-ROW_LIM_ALL = 100
+STEP = 3
+ROW_LIM_STOP = int(120*2/STEP)
+ROW_LIM_ALL = int(100*2/STEP)
 
 SEC_LIM_STOP = 5
 SEC_LIM_ALL = 0
 
 def detect(img):
 
-	img_sample_stop = img[370:400:2,int(constants.W*2/5):int(constants.W*3/5):2]
+	img_sample_stop = img[370:400:STEP,int(constants.W*1/5):int(constants.W*4/5):STEP]
 	rows_stop = img_sample_stop.shape[0]
 
-	img_sample_all = np.vstack([img[int(constants.H/2):370:2,int(constants.W*2/5):int(constants.W*3/5):2], img[400:constants.H:2,int(constants.W*2/5):int(constants.W*3/5):2]])
+	img_sample_all = np.vstack([img[int(constants.H/2):370:STEP,int(constants.W*1/5):int(constants.W*4/5):STEP], img[400:constants.H:STEP,int(constants.W*1/5):int(constants.W*4/5):STEP]])
 	rows_all = img_sample_all.shape[0]
 
 	all_rows_stop = [sum([1 for pix in img_sample_stop[incr] if (pix[2] > 245 and pix[1] < 175 and pix[0] < 175)]) for incr in range(0, rows_stop)]
@@ -23,13 +24,5 @@ def detect(img):
 
 	return [sum([1 for incr in range(0, rows_stop-1) if (all_rows_stop[incr] + all_rows_stop[incr+1]) > ROW_LIM_STOP]) > SEC_LIM_STOP,
 		 sum([1 for incr in range(0, rows_all-1) if (all_rows_all[incr] + all_rows_all[incr+1]) > ROW_LIM_ALL]) > SEC_LIM_ALL]
-
-INSIDE_LIM = 1200
-
-def inside(img):
-
-	img_sample = img[int(constants.H*4/5):int(constants.H):2,int(constants.W*2/5):int(constants.W*3/5):2]
-
-	return sum([1 for dim in img_sample for pix in dim if (pix[2] > 245 and pix[1] < 175 and pix[0] < 175)]) 
 
 
--- comp/src/control/pid.py
+++ comp/src/control/pid.py
@@ -35,3 +35,27 @@ def update(move, state):
         move.linear.x = 0
         move.angular.z = -1.3*CONST_ANG
 
+# P = 2
+# P_ANG = 2
+
+# BASE_VEL = 0.1*P
+# SCALE_VEL = 0.045*P
+
+# SCALE_ANG = 0.06*P_ANG*P
+# CONST_ANG = 1.2
+# # CONST_ANG = 0.2*P_ANG*P
+
+# def update(move, state):
+    
+#     if(not sum(state) is -2):
+#     	diff = state[1] - state[0]
+#         move.linear.x = BASE_VEL - abs(diff)*SCALE_VEL
+#         if(diff > 0):
+#         	move.angular.z = SCALE_ANG
+#         	# move.angular.z = SCALE_ANG*(abs(diff)**(1.0/3.0))
+#         else:
+#         	move.angular.z = -SCALE_ANG
+#         	# move.angular.z = -SCALE_ANG*(abs(diff)**(1.0/3.0))
+#     else:
+#         move.linear.x = -BASE_VEL
+#         move.angular.z = -CONST_ANG
